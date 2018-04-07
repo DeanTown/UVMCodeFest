4823c1250647e37088ed049b617b551e0eb7111d
karissaperkins <kperkin2@uvm.edu>
2018-04-07 14:19:27 -0400

    CSS

--- top.php
+++ top.php
@@ -7,3 +7,15 @@
  */
 
 include 'header.php';
+
+?>
+<html lang="en">
+    <head>
+        <title>Dorm Match</title>
+
+        <meta charset="utf-8">
+        <meta name="author" content="">
+        <meta name="description" content="top of page">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/CodeFest2018/CSS/style.css" type="text/css" media="screen">
+    </head>
