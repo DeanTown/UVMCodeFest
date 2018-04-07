0d84777d5f5277b90c6fba62dba930a2ba92c2da
Ardsta <smartymcsmartle@gmail.com>
2018-04-07 12:58:50 -0400

    Changed redstone hall image name

---
+++ campus-detail.php
@@ -0,0 +1,83 @@
+<?php
+$campus = "";
+if (isset($_GET['campus'])) {
+    $campus = htmlentities($_GET['campus'], ENT_QUOTES, "UTF-8");
+}
+// *** Open data *** //
+$debug = false;
+if (isset($_GET["debug"])) {
+    $debug = true;
+}
+
+$myFolder = '';
+$myFileName = 'dorms';
+$fileExt = '.csv';
+$filename = $myFolder . $myFileName . $fileExt;
+
+if ($debug)
+    print '<p>filename is ' . $filename;
+
+$file = fopen($filename, "r");
+
+if ($debug) {
+    if ($file) {
+        print '<p>File Opened Succesful.</p>';
+    } else {
+        print '<p>File Open Failed.</p>';
+    }
+}
+
+// *** Read Data *** //
+if ($file) {
+    if ($debug)
+        print '<p>Begin reading data into an array.</p>';
+
+    // read the header row, copy the line for each header row
+    // you have.
+    $headers[] = fgetcsv($file);
+
+    if ($debug) {
+        print '<p>Finished reading headers.</p>';
+        print '<p>My header array</p><pre>';
+        print_r($headers);
+        print '</pre>';
+    }
+
+    // read all the data
+    while (!feof($file)) {
+        $dormDetails[] = fgetcsv($file);
+    }
+
+    if ($debug) {
+        print '<p>Finished reading data. File closed.</p>';
+        print '<p>My data array<p><pre> ';
+        print_r($dormDetails);
+        print '</pre></p>';
+    }
+} // ends if file was opened 
+
+fclose($file);
+include('top.php');
+?>
+<article id="dorm">
+    <h2><?php print $campus;?> Dorms</h2>
+    <?php
+        foreach ($dormDetails as $dormDetail) {
+            if ($campus == str_replace(' ', '', $dormDetail[0])) {
+                print '<figure class="dorm"><a class="dorm-link" href="dorm-detail.php?dorm=';
+                print str_replace(' ', '', $dormDetail[1]);
+                print '">';
+                print '<img class="dorm-link" alt="' . $dormDetail[1];
+                print '" src="images/' . str_replace(' ', '', $dormDetail[1]);
+                print '.jpg">';
+                print '</a><figcaption>' . $dormDetail[1];
+                print '</figcaption></figure>';
+            }
+        }   
+    ?>
+</article>
+<?php
+include('footer.php');
+?>
+</body>
+</html>
